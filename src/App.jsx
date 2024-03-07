613343bec202ec46cd93381e9b0015bfea064ed7
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-07 09:02:02 +0600

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ResetPassword from "./pages/ResetPassword";
 
 export default function () {
   return (
     <Router>
       <div>
+      {/* <Home/> */}
         <Routes>
-          <Route />
+          <Route path="/" element={<Home/>} index />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/register" element={<RegisterPage/>} />
+          <Route path="/resetPassword" element={<ResetPassword/>} />
         </Routes>
       </div>
     </Router>
